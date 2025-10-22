6884b78abbc713b685dfa3affc191053c68070b9
Carl Lang <carl.m.lang@gmail.com>
2025-10-22 20:56:43 +0100

    add google sheets as persitance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
-import { metadata } from "./metadata";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export { metadata };
 
 export default function RootLayout({
   children,
@@ -17,16 +17,26 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-400 min-h-screen bg-gradient-to-b bg-[#121212] font-sans text-slate-300`}
       >
-        <div className="fixed left-0 top-0 -z-10 h-full w-full">
-          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        </div>
+        <GoogleReCaptchaProvider
+          reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+          scriptProps={{
+            async: false,
+            defer: false,
+            appendTo: "head",
+            nonce: undefined,
+          }}
+        >
+          <div className="fixed left-0 top-0 -z-10 h-full w-full">
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+          </div>
 
-        <Navigation />
+          <Navigation />
 
-        {/* Main Content */}
-        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mt-20 md:mt-24">{children}</div>
-        </main>
+          {/* Main Content */}
+          <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="mt-20 md:mt-24">{children}</div>
+          </main>
+        </GoogleReCaptchaProvider>
       </body>
     </html>
   );
